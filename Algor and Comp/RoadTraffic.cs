b28065fa898f10f1cc4d61e7f1c92d17160ac13f
thev26 <121070237+thev26@users.noreply.github.com>
2023-03-22 15:27:24 +0000

    Traffic Data AnC

---
+++ Algor and Comp/RoadTraffic.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace RoadTrafficData
+{
+    class Program
+    {
+        static void Main()
+        {
+            // Read files into arrays
+            int[] road1Data = File.ReadAllLines("Road_1_256.txt")
+                                  .Select(int.Parse)
+                                  .ToArray();
+
+            int[] road2Data = File.ReadAllLines("Road_2_256.txt")
+                                  .Select(int.Parse)
+                                  .ToArray();
+
+            int[] road3Data = File.ReadAllLines("Road_3_256.txt")
+                                  .Select(int.Parse)
+                                  .ToArray();
+
+            // Sort arrays in ascending order
+            Array.Sort(road1Data);
+            Array.Sort(road2Data);
+            Array.Sort(road3Data);
+
+            // Display every 10th value of the sorted arrays in ascending order
+            Console.WriteLine("Road_1_256.txt - Sorted in ascending order:");
+            for (int i = 0; i < road1Data.Length; i += 10)
+            {
+                Console.WriteLine(road1Data[i]);
+            }
+
+            Console.WriteLine("\nRoad_2_256.txt - Sorted in ascending order:");
+            for (int i = 0; i < road2Data.Length; i += 10)
+            {
+                Console.WriteLine(road2Data[i]);
+            }
+
+            Console.WriteLine("\nRoad_3_256.txt - Sorted in ascending order:");
+            for (int i = 0; i < road3Data.Length; i += 10)
+            {
+                Console.WriteLine(road3Data[i]);
+            }
+
+            // Sort arrays in descending order
+            Array.Reverse(road1Data);
+            Array.Reverse(road2Data);
+            Array.Reverse(road3Data);
+
+            // Display every 10th value of the sorted arrays in descending order
+            Console.WriteLine("\nRoad_1_256.txt - Sorted in descending order:");
+            for (int i = 0; i < road1Data.Length; i += 10)
+            {
+                Console.WriteLine(road1Data[i]);
+            }
+
+            Console.WriteLine("\nRoad_2_256.txt - Sorted in descending order:");
+            for (int i = 0; i < road2Data.Length; i += 10)
+            {
+                Console.WriteLine(road2Data[i]);
+            }
+
+            Console.WriteLine("\nRoad_3_256.txt - Sorted in descending order:");
+            for (int i = 0; i < road3Data.Length; i += 10)
+            {
+                Console.WriteLine(road3Data[i]);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Algor and Comp/TrafficData.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        // Call the ReadFile method to read the files into arrays
+        int[] road1 = ReadFile("Road_1_256.txt");
+        int[] road2 = ReadFile("Road_2_256.txt");
+        int[] road3 = ReadFile("Road_3_256.txt");
+
+        // Call the DisplayEveryTenthValue method to display every 10th value in the arrays
+        Console.WriteLine("Every 10th value in Road 1:");
+        DisplayEveryTenthValue(road1);
+
+        Console.WriteLine("Every 10th value in Road 2:");
+        DisplayEveryTenthValue(road2);
+
+        Console.WriteLine("Every 10th value in Road 3:");
+        DisplayEveryTenthValue(road3);
+
+        // Call the SortArrayAscending and SortArrayDescending methods to sort the arrays
+        SortArrayAscending(road1);
+        SortArrayDescending(road2);
+
+        // Call the DisplayEveryTenthValue method again to display every 10th value in the sorted arrays
+        Console.WriteLine("Every 10th value in Road 1 (sorted in ascending order):");
+        DisplayEveryTenthValue(road1);
+
+        Console.WriteLine("Every 10th value in Road 2 (sorted in descending order):");
+        DisplayEveryTenthValue(road2);
+    }
+
+    static int[] ReadFile(string fileName)
+    {
+        // Add code here to read the file and return an array
+    }
+
+    static void DisplayEveryTenthValue(int[] arr)
+    {
+        // Add code here to display every 10th value in the array
+    }
+
+    static void SortArrayAscending(int[] arr)
+    {
+        // Add code here to sort the array in ascending order
+    }
+
+    static void SortArrayDescending(int[] arr)
+    {
+        // Add code here to sort the array in descending order
+    }
+}
